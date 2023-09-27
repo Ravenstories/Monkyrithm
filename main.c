2ab6788a016fc635d2e3a9cbe3f46205f2f68ad3
Jacob <henriksen.jacob@gmail.com>
2023-09-27 09:19:23 +0200

    selectionSort

--- main.c
+++ main.c
@@ -8,7 +8,7 @@ int main()
     srand(time(0));
 
     // Antallet af elementer i arrayet
-    int size = 100000;
+    int size = 100;
 
     // Allokerer hukommelse til arrayet
     int *arr = malloc(size * sizeof(int));
@@ -18,7 +18,7 @@ int main()
     }
 
     // Definerer de nedre og øvre grænser for tilfældige tal
-    int lower = 1, upper = 100000;
+    int lower = 1, upper = 1000;
 
     // Genererer tilfældige tal og gemmer dem i arrayet
     generateRandomNumbers(arr, size, lower, upper);
@@ -28,7 +28,14 @@ int main()
     for (int i = 0; i < size; ++i) {
         printf("%d ", arr[i]);
     }
-    printf("\n");
+    printf("\n\n");
+
+    // Sorterer arrayet
+    selectionSort(arr, size);
+    printf("Sorterede tal i arrayet: ");
+    for (int i = 0; i < size; ++i) {
+        printf("%d ", arr[i]);
+    }
 
     // Frigør hukommelsen
     free(arr);
@@ -41,3 +48,25 @@ void generateRandomNumbers(int *arr, int size, int lower, int upper) {
         arr[i] = (rand() % (upper - lower + 1)) + lower;
     }
 }
+
+void selectionSort(int *arr, int size){
+    int i, j, min_idx;
+
+    for (i = 0; i < size-1; i++)
+    {
+        min_idx = i;
+        for (j = i+1; j < size; j++)
+            if (arr[j] < arr[min_idx])
+                min_idx = j;
+
+        swap(&arr[min_idx], &arr[i]);
+    }
+}
+
+void swap(int *xp, int *yp)
+{
+    int temp = *xp;
+    *xp = *yp;
+    *yp = temp;
+}
+
