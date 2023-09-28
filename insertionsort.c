39adf9fbe5cd25577b33b3c841ee794f59fb8bc9
Thomas Carl Alexander Dänhardt <thomas.danhardt@gmail.com>
2023-09-28 08:47:50 +0200

    added comments to insertionsort.c

--- insertionsort.c
+++ insertionsort.c
@@ -2,9 +2,11 @@
 #include <stdlib.h>
 
 void insertionSort (int arr[], int size) {
-
+    // I is the main iteration, and we start at position 1, so we can always compare backwards (so we start comparing position 1 to position 0)
     for (int i = 1; i < size; i++) {
+        // j is for iterating through the sorted part of the array, thus it does not exceed i.
         for (int j = 0; j < i; j++) {
+            // The first time it encounters a value that is greater than itself, have it "inserted", aka. moved down the array to that position
             if (arr[i] < arr[j]) {
                 moveIntDownArray(arr, j, i);
                 break;
@@ -14,6 +16,7 @@ void insertionSort (int arr[], int size) {
 }
 
 void moveIntDownArray(int arr[], int insertIndex, int insertOrigin) {
+    // i starts at the insertOrigin index, and will continue moving until it reaches the insertion index, swapping each pair value along the way
     for (int i = insertOrigin; i > insertIndex; i--) {
         swap(&arr[i], &arr[i-1]);
     }
